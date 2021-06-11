62fe28c2409f2345e698b356482d9e52495ab784
krett <krett.off@gmail.com>
2021-06-11 14:33:20 +0300

    create the method plus

--- src/task06/exercise01/Calculator.java
+++ src/task06/exercise01/Calculator.java
@@ -3,9 +3,11 @@ package task06.exercise01;
 public class Calculator {
 
     public static void main(String[] args) {
-
+        Calculator calc = new Calculator();
+        System.out.printf("%s", calc.plus(7, 8));
     }
 
     public int plus(int a, int b) {
+        return a + b;
     }
 }
