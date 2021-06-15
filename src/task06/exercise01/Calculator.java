a01d213c642c0a1a06c449afe426841daca8377d
krett <krett.off@gmail.com>
2021-06-15 11:56:00 +0300

    change the method plus, on the method multiply

--- src/task06/exercise01/Calculator.java
+++ src/task06/exercise01/Calculator.java
@@ -4,10 +4,10 @@ public class Calculator {
 
     public static void main(String[] args) {
         Calculator calc = new Calculator();
-        System.out.printf("%s", calc.plus(7, 8));
+        System.out.printf("%s", calc.multiply(7.4, 8.6));
     }
 
-    public int plus(int a, int b) {
+    public double multiply(double a, double b) {
         return a + b;
     }
 }
