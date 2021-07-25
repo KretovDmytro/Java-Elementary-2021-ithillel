072bc4326bd3711afcd42a006d5cd5048dba0554
krett <krett.off@gmail.com>
2021-07-25 13:17:32 +0300

    do exercise 2 in task09

---
+++ src/task09/exercise02/DuplicatLists.java
@@ -0,0 +1,39 @@
+package task09.exercise02;
+
+import java.util.*;
+
+public class DuplicatLists {
+
+    List<String> stringList = new ArrayList<>();
+
+    public void addToTheList(String... string) {
+        stringList.addAll(Arrays.asList(string));
+    }
+
+    public List<String> removeDuplicatesWithSet(List<String> list) {
+        Set<String> newList = new HashSet<>(list);
+        return new ArrayList<>(newList);
+    }
+
+    public List<String> removeDuplicatesWithoutSet(List<String> list) {
+
+        List<String> newList = new ArrayList<>(list);
+
+        Iterator<String> iterator = newList.iterator();
+        while (iterator.hasNext()) {
+            String s = iterator.next();
+            int count = 0;
+            for (String value : newList) {
+                if (s.equals(value)) {
+                    count++;
+                    if (count > 1) {
+                        iterator.remove();
+                        break;
+                    }
+                }
+
+            }
+        }
+        return newList;
+    }
+}
--- src/task09/exercise02/Main.java
+++ src/task09/exercise02/Main.java
@@ -11,5 +11,13 @@ public class Main {
 
     public static void main(String[] args) {
 
+        DuplicatLists object = new DuplicatLists();
+
+        object.addToTheList("qwerty", "qwerty", "qwerty1", "ASD", "ASD", "ASD", " . ", "ASD", "2", "2", "2", "puc", "2s", "2s"); // add
+
+        System.out.println("check " + object.stringList); // check
+        System.out.println("With \"Set\": " + object.removeDuplicatesWithSet(object.stringList));
+        System.out.println("Without \"Set\": " + object.removeDuplicatesWithoutSet(object.stringList));
+        System.out.println("check " + object.stringList); // check
     }
 }
